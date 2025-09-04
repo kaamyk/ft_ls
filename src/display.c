93ec867656ac9546d591c5ea2970c88e351ded4f
Antoine VINCENT <anvincen@f6r11s6.clusters.42paris.fr>
2025-09-04 16:29:12 +0200

    -l content OK EXCEPT total blocks

--- src/display.c
+++ src/display.c
@@ -35,35 +35,35 @@ void	display_rights(t_file_list *entry)
 	write(STDOUT_FILENO, " ", 1);
 }
 
-void	display_group_user_name(t_file_list *entry, uint8_t alignment[3])
+void	display_group_user_name(t_file_list *entry, uint8_t long_format_data[4])
 {
 	struct passwd	*us = getpwuid(entry->file.stat.st_uid);
 	struct group	*gr = getgrgid(entry->file.stat.st_gid);
 
-	ft_printf("%*s %*s ", alignment[0], us->pw_name, alignment[1], gr->gr_name);
+	ft_printf("%*s %*s ", long_format_data[0], us->pw_name, long_format_data[1], gr->gr_name);
 }
 
 void	display_date(t_file_list *entry)
 {
-	time_t	raw_time = (entry->file.stat.st_mtim.tv_sec * 1000000) + entry->file.stat.st_mtim.tv_nsec;
+	time_t	raw_time = entry->file.stat.st_mtim.tv_sec; 
 	char	*str = ctime(&raw_time);
 	
 	ft_printf("%.12s ", str + 4);
 }
 
-void	display_long_format(t_file_list *entry, uint8_t alignment[3])
+void	display_long_format(t_file_list *entry, uint8_t long_format_data[4])
 {
 	display_type(entry);
 	display_rights(entry);
-	ft_printf("%.*ld ", alignment[3],entry->file.stat.st_nlink);
-	display_group_user_name(entry, alignment);
-	ft_printf("%*ld ", alignment[2], entry->file.stat.st_size);
+	ft_printf("%.*ld ", long_format_data[3],entry->file.stat.st_nlink);
+	display_group_user_name(entry, long_format_data);
+	ft_printf("%*ld ", long_format_data[2], entry->file.stat.st_size);
 	display_date(entry);
 	write(STDOUT_FILENO, entry->file.dirent.d_name, ft_strlen(entry->file.dirent.d_name));
 	write(STDOUT_FILENO, "\n", 1);
 }
 
-void	get_alignment_values(t_file_list *entries, uint8_t alignment[4])
+void	get_long_format_data(t_file_list *entries, uint8_t long_format_data[4], uint32_t *total_blocks)
 {
 	t_file_list	*runner = entries;
 	int			tmp = 0;
@@ -74,14 +74,15 @@ void	get_alignment_values(t_file_list *entries, uint8_t alignment[4])
 	{
 		us = getpwuid(runner->file.stat.st_uid);
 		gr = getgrgid(runner->file.stat.st_gid);
-		if (ft_strlen(us->pw_name) > alignment[0])
-			alignment[0] = ft_strlen(us->pw_name);
-		if ((tmp = ft_strlen(gr->gr_name)) > alignment[1])
-			alignment[1] = tmp;
-		if ((tmp = ft_nblen(runner->file.stat.st_size)) > alignment[2])
-			alignment[2] = tmp;
-		if ((tmp = ft_nblen(runner->file.stat.st_nlink)) > alignment[3])
-			alignment[3] =	tmp;
+		if (ft_strlen(us->pw_name) > long_format_data[0])
+			long_format_data[0] = ft_strlen(us->pw_name);
+		if ((tmp = ft_strlen(gr->gr_name)) > long_format_data[1])
+			long_format_data[1] = tmp;
+		if ((tmp = ft_nblen(runner->file.stat.st_size)) > long_format_data[2])
+			long_format_data[2] = tmp;
+		if ((tmp = ft_nblen(runner->file.stat.st_nlink)) > long_format_data[3])
+			long_format_data[3] = tmp;
+		*total_blocks += runner->file.stat.st_blocks;
 		runner = runner->next;
 	}
 }
@@ -89,15 +90,21 @@ void	get_alignment_values(t_file_list *entries, uint8_t alignment[4])
 void	display(t_file_list *entries, bool long_format, bool list_all)
 {
 	t_file_list	*runner 	= entries;
-	uint8_t		alignment[4] = {0};		// 0: username, 1: groupanme, 2: size, 3: nblink
+	uint8_t		long_format_data[4] = {0};		// 0: username, 1: groupanme, 2: size, 3: nb link
+	// finir le calcul de total blocks.
+	uint32_t	total_blocks = 0;
 
-	get_alignment_values(entries, alignment);
+	get_long_format_data(entries, long_format_data, &total_blocks);
+	if (long_format == 1)
+		ft_printf("total %d\n",total_blocks); 
 	while (runner != NULL)
 	{
 		if (list_all == 1 || runner->file.dirent.d_name[0] != '.')
 		{
 			if (long_format == 1)
-				display_long_format(runner, alignment);
+			{
+				display_long_format(runner, long_format_data);
+			}
 			else
 				ft_printf("%s ", runner->file.dirent.d_name);
 		}
